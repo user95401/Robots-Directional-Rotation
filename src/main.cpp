978af7c69f378ff0dfad59810e4480adf3a981ad
user95401 <90561697+user95401@users.noreply.github.com>
2025-01-23 16:06:16 +0400

    паблик

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <_main.hpp>
-#include <_updater.hpp>
+
+//extra hardcode goes here
 
 #include <Geode/modify/PlayerObject.hpp>
 class $modify(PlayerObjectExt, PlayerObject) {
 	$override void updateRotation(float p0) {
 		if ((m_isRobot or m_isSpider or SETTING(bool, "For all game modes")) and !(m_isOnGround and !SETTING(bool, "Don't care if is On Ground"))) {
 
-			auto org_isSwing = m_isSwing;
-			auto org_isBird = m_isBird;
+			auto wasSwing = m_isSwing;
+			auto wasisBird = m_isBird;
 
 			if (SETTING(bool, "Use bird rotation")) m_isBird = true;
 			else m_isSwing = true;
 
 			PlayerObject::updateRotation(p0 * SETTING(double, "Delta Multiplier"));
 
-			m_isSwing = org_isSwing;
-			m_isBird = org_isBird;
+			m_isSwing = wasSwing;
+			m_isBird = wasBird;
 
 			return;
 		};
